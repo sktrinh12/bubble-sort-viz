0609265632e6634b6c42e57bbe9dfbafcb4c797b
sktrinh12 <sktrinh12@gmail.com>
2023-02-05 00:50:08 -0800

    fixed sort function, added reset handle and altered number change handle

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,101 @@
 import { useState, createRef, useEffect, useRef, RefObject } from "react";
 import GridLayout from "./Grid";
+import Rectangle from "./Rectangle";
 
+const highlight = "#296178";
 const RectangleGenerator = () => {
-  const [numRectangles, setNumRectangles] = useState(0);
-  const [colour, setColour] = useState("#70d0d1");
+  const [numRectangles, setNumRectangles] = useState<number>(0);
+  const [colour, setColour] = useState<string>("#4e4a8e");
   const svgRef = useRef<SVGSVGElement>(null);
+  const [isDisabled, setIsDisabled] = useState<boolean>(false);
   const [rectangles, setRectangles] = useState<
     { element: JSX.Element; height: number }[]
   >([]);
-  const svgElRefs = useRef<Array<RefObject<SVGRectElement>>>([]);
   const [rectRefs, setRectRefs] = useState<Array<RefObject<SVGRectElement>>>(
     []
   );
 
+  const handleClickReset = () => {
+    setIsDisabled(false);
+    setNumRectangles(0);
+  };
+
+  const handleChange = (event: any) => {
+    if (event.target.value > -1) {
+      setNumRectangles(event.target.value);
+    } else {
+      setNumRectangles(0);
+    }
+  };
+
   const handleClickSort = () => {
+    let delay = 0;
+    let n = rectRefs.length;
     let arr = [...rectangles];
-    let n = arr.length;
+    setIsDisabled(true);
     for (let i = 0; i < n - 1; i++) {
       for (let j = 0; j < n - i - 1; j++) {
-        if (arr[j].height > arr[j + 1].height) {
-          let temp = arr[j];
-          let currentRect = svgElRefs.current[j].current;
-          arr[j] = arr[j + 1];
-          arr[j + 1] = temp;
-          let subseqRect = svgElRefs.current[j + 1].current;
-          if (currentRect && subseqRect) {
-            currentRect.setAttribute(
-              "x",
-              parseFloat(subseqRect.getAttribute("x") ?? "0").toString()
-            );
-            currentRect.setAttribute(
-              "y",
-              parseFloat(subseqRect.getAttribute("y") ?? "0").toString()
-            );
-            subseqRect = currentRect;
+        setTimeout(() => {
+          const currRect = rectRefs[j].current;
+          const nextRect = rectRefs[j + 1].current;
+          if (arr[j].height > arr[j + 1].height) {
+            const currX = currRect.getAttribute("x");
+            const nextX = nextRect.getAttribute("x");
+            currRect.setAttribute("fill", `${highlight}`);
+            nextRect.setAttribute("fill", `${highlight}`);
+            setTimeout(() => {
+              currRect.setAttribute("fill", `${colour}`);
+              nextRect.setAttribute("fill", `${colour}`);
+            }, 350);
+            currRect.setAttribute("x", nextX.toString());
+            nextRect.setAttribute("x", currX.toString());
+            let temp = rectRefs[j];
+            rectRefs[j] = rectRefs[j + 1];
+            rectRefs[j + 1] = temp;
+            temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
           }
-        }
+        }, delay);
+        delay += 400;
       }
     }
-    setRectangles(arr);
-    console.log(arr);
   };
 
   useEffect(() => {
     const genRect = () => {
       const rectangles = [];
+      const newRectRefs = [];
       const boundingBox = svgRef.current!.getBoundingClientRect();
       const yPos = boundingBox.height;
       const xPos = boundingBox.width;
       const xWidth = numRectangles > 1 ? xPos / numRectangles : xPos;
-      // console.log(xWidth);
 
       for (let i = 0; i < numRectangles; i++) {
         const randomHeight = Math.random() * yPos;
         const rectRef = createRef<SVGRectElement>();
         const yHeight = yPos - randomHeight;
-        svgElRefs.current.push(rectRef);
-        // console.log(randomHeight);
-        setRectRefs((prevRectRefs) => [...prevRectRefs, rectRef]);
         rectangles.push({
           element: (
-            <rect
-              key={i}
-              ref={rectRef}
-              x={i * xWidth}
-              y={randomHeight}
-              width={xWidth}
-              height={yHeight}
-              fill={colour}
-              stroke={"black"}
-              strokeWidth={0.2}
+            <Rectangle
+              index={i}
+              rectRef={rectRef}
+              xWidth={xWidth}
+              randomHeight={randomHeight}
+              yHeight={yHeight}
+              colour={colour}
             />
           ),
           height: yHeight,
         });
+        newRectRefs.push(rectRef);
       }
       setRectangles(rectangles);
+      setRectRefs(newRectRefs);
     };
+
     genRect();
   }, [numRectangles]);
-
-  const handleChange = (event: any) => {
-    setNumRectangles(event.target.value);
-  };
 
   return (
     <GridLayout
@@ -91,6 +104,8 @@ const RectangleGenerator = () => {
       numRectangles={numRectangles}
       rectangles={rectangles.map((rect) => rect.element)}
       svgRef={svgRef}
+      isDisabled={isDisabled}
+      handleClickReset={handleClickReset}
     />
   );
 };
